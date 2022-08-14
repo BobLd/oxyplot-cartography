e9abb7c1400a8447af0b824d253edab2a86c4ae8
BobLD <boris.ledeist@edhec.com>
2022-08-14 10:58:21 +0100

    Update SampleDemo

--- SimpleDemo/ViewModels/MapViewModel.cs
+++ SimpleDemo/ViewModels/MapViewModel.cs
@@ -57,6 +57,7 @@ namespace SimpleDemo.ViewModels
                 Minimum = -0.24,
                 Maximum = 0.04,
                 Title = "Longitude",
+                //LabelFormatter = (decDegrees) => CartographyHelper.DecimalDegreesToDegreesMinutesSeconds(decDegrees, false, 3)
             });
 
             model.Axes.Add(new LatitudeWebMercatorAxis
@@ -65,12 +66,16 @@ namespace SimpleDemo.ViewModels
                 Minimum = 51.42,
                 Maximum = 51.62,
                 Title = "Latitude",
+                //LabelFormatter = (decDegrees) => CartographyHelper.DecimalDegreesToDegreesMinutesSeconds(decDegrees, true, 3)
             });
 
             var tileMapImageProvider = new HttpTileMapImageProvider(SynchronizationContext.Current)
             {
                 //Url = "https://www.metoffice.gov.uk/public/tiles/map/{Z}/{Y}/{X}.png",
-                Url = "http://tile.openstreetmap.org/{Z}/{X}/{Y}.png",
+                //Url = "https://tile-c.openstreetmap.fr/hot/{Z}/{X}/{Y}.png", // Humanitarian
+                //Url = "https://gps.tile.openstreetmap.org/lines/{Z}/{X}/{Y}.png", // Public GPS trace
+                Url = "http://tile.openstreetmap.org/{Z}/{X}/{Y}.png", // Standard
+                //Url = "https://b.tile-cyclosm.openstreetmap.fr/cyclosm/{Z}/{X}/{Y}.png", // CyclOSM
                 //Url = "https://server.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{Z}/{Y}/{X}", // https://developers.arcgis.com/documentation/mapping-apis-and-services/data-hosting/services/image-tile-service/
                 //Url = "https://maptiles.finncdn.no/tileService/1.0.3/norortho/{Z}/{X}/{Y}.png",
                 //Url = "https://maptiles.finncdn.no/tileService/1.0.3/normap/{Z}/{X}/{Y}.png",
@@ -101,22 +106,6 @@ namespace SimpleDemo.ViewModels
                 })
             };
 
-            /*
-            var tileMapImageProvider = new LocalTileMapImageProvider()
-            {
-                ImageConverter = new Func<byte[], byte[]>(input =>
-                {
-                    using (var msInput = new MemoryStream(input))
-                    using (var msOutput = new MemoryStream())
-                    {
-                        var bitmap = Bitmap.DecodeToWidth(msInput, 256);
-                        bitmap.Save(msOutput);
-                        return msOutput.ToArray();
-                    }
-                })
-            };
-            */
-
             var loadingImg = new Uri("avares://SimpleDemo/Assets/live-view.png");
             var asset = AvaloniaLocator.Current.GetService<IAssetLoader>();
             using (var streamImg = asset.Open(loadingImg))
@@ -131,6 +120,53 @@ namespace SimpleDemo.ViewModels
                     TileGridThickness = 3
                 });
             }
+
+            /*
+            // Public GPS trace
+            var tileMapImageProvider2 = new HttpTileMapImageProvider(SynchronizationContext.Current)
+            {
+                //Url = "https://www.metoffice.gov.uk/public/tiles/map/{Z}/{Y}/{X}.png",
+                //Url = "https://tile-c.openstreetmap.fr/hot/{Z}/{X}/{Y}.png", // Humanitarian
+                Url = "https://gps.tile.openstreetmap.org/lines/{Z}/{X}/{Y}.png", // Public GPS trace
+                //Url = "http://tile.openstreetmap.org/{Z}/{X}/{Y}.png", // Standard
+                //Url = "https://server.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{Z}/{Y}/{X}", // https://developers.arcgis.com/documentation/mapping-apis-and-services/data-hosting/services/image-tile-service/
+                //Url = "https://maptiles.finncdn.no/tileService/1.0.3/norortho/{Z}/{X}/{Y}.png",
+                //Url = "https://maptiles.finncdn.no/tileService/1.0.3/normap/{Z}/{X}/{Y}.png",
+
+                MaxNumberOfDownloads = 2,
+                UserAgent = "OxyPlot.Cartography",
+                ImageConverter = new Func<byte[], byte[]>(bytes =>
+                {
+                    // https://github.com/oxyplot/oxyplot/blob/205e968870c292ecaeab2cb9e7f34904897126cb/Source/OxyPlot/Imaging/OxyImage.cs#L221
+                    if (bytes.Length >= 2 && bytes[0] == 0x42 && bytes[1] == 0x4D)
+                    {
+                        return bytes; // Bmp
+                    }
+
+                    if (bytes.Length >= 4 && bytes[0] == 0x89 && bytes[1] == 0x50 && bytes[2] == 0x4E && bytes[3] == 0x47)
+                    {
+                        return bytes; //Png
+                    }
+
+                    using (var msInput = new MemoryStream(bytes))
+                    using (var msOutput = new MemoryStream())
+                    {
+                        var bitmap = Bitmap.DecodeToWidth(msInput, 256);
+                        bitmap.Save(msOutput);
+                        return msOutput.ToArray();
+                    }
+                    return bytes;
+                })
+            };
+
+            // Add the tile map annotation
+            model.Annotations.Add(new MapTileAnnotation(tileMapImageProvider2)
+            {
+                CopyrightNotice = "© OpenStreetMap contributors",
+                MinZoomLevel = 0,
+                MaxZoomLevel = 19, // max OpenStreetMap value
+            });
+            */
 
             return model;
         }
