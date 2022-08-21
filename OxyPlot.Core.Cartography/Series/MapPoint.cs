fd18ae8789909e51ea2b59594ea32266e890ee3b
BobLD <boris.ledeist@edhec.com>
2022-08-21 12:43:11 +0100

    Update MapPoint

--- OxyPlot.Core.Cartography/Series/MapPoint.cs
+++ OxyPlot.Core.Cartography/Series/MapPoint.cs
@@ -9,19 +9,69 @@ namespace OxyPlot.Series
     public struct MapPoint
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="MapPoint"/> struct.
+        /// <para>
+        /// The latitude and longitude will be set to <see cref="double.NaN"/>, the altitude to null.
+        /// </para>
+        /// </summary>
+        public MapPoint()
+        {
+            Latitude = double.NaN;
+            Longitude = double.NaN;
+            Altitude = null;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MapPoint"/> struct.
+        /// <para>
+        /// The altitude will be set to null.
+        /// </para>
+        /// </summary>
+        /// <param name="latitude">The latitude (Y coordinate)</param>
+        /// <param name="longitude">The longitude (X coordinate)</param>
+        public MapPoint(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            Altitude = null;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MapPoint"/> struct.
+        /// </summary>
+        /// <param name="latitude">The latitude (Y coordinate)</param>
+        /// <param name="longitude">The longitude (X coordinate)</param>
+        /// <param name="altitude">The altitude (Z coordinate)</param>
+        public MapPoint(double latitude, double longitude, double altitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            Altitude = altitude;
+        }
+
+        /// <summary>
         /// Gets the latitude (Y coordinate).
         /// </summary>
-        public double Latitude { get; init; }
+        public double Latitude { get; }
 
         /// <summary>
         /// Gets the longitude (X coordinate).
         /// </summary>
-        public double Longitude { get; init; }
+        public double Longitude { get; }
 
         /// <summary>
         /// Gets the altitude (Z coordinate).
         /// </summary>
-        public double? Altitude { get; init; }
+        public double? Altitude { get; }
+
+        /// <summary>
+        /// Determines if the <see cref="Altitude"/> is defined.
+        /// </summary>
+        /// <returns><c>true</c> if this <see cref="Altitude"/> is defined; otherwise, <c>false</c>.</returns>
+        public bool HasAltitude()
+        {
+            return Altitude.HasValue;
+        }
 
         /// <inheritdoc/>
         public override bool Equals([NotNullWhen(true)] object? obj)
@@ -50,12 +100,12 @@ namespace OxyPlot.Series
         /// </summary>
         public override string ToString()
         {
-            if (!Altitude.HasValue)
+            if (Altitude.HasValue)
             {
-                return $"{Latitude}, {Longitude}";
+                return $"{Latitude}, {Longitude}, {Altitude}";
             }
 
-            return $"{Latitude}, {Longitude}, {Altitude}";
+            return $"{Latitude}, {Longitude}";
         }
 
         /// <inheritdoc/>
